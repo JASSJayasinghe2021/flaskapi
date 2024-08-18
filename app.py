e837a9bfb40bd763e14bf8279f54767b0205d9f5
JASSJayasinghe2021 <vovolabs@gmail.com>
2024-08-18 14:43:38 +0900

    initial server

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
+
+app = Flask(__name__)
+COUNTRIES = [
+        {"id":1, "title":"Sri Lank"},
+        {"id":2, "title":"India"},
+        {"id":3, "title":"Pakistan"}
+        ]
+
+@app.route("/")
+def hello_world():
+    return render_template('index.html', countries=COUNTRIES)
+
+@app.route("/countries")
+def get_countries():
+    return jsonify(COUNTRIES)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
